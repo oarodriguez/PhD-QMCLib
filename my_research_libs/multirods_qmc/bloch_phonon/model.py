2d1cade0889a434500b5bd0db77e0ce7841677ea
Omar Abel Rodríguez-López <oarodriguez.mx@gmail.com>
2018-11-11 03:31:18 -0600

    ✨ 🎨 🔨 ✅  Update the VMC sampling of Jastrow models to use named tuples

--- my_research_libs/multirods_qmc/bloch_phonon/model.py
+++ my_research_libs/multirods_qmc/bloch_phonon/model.py
@@ -3,7 +3,7 @@ from abc import ABCMeta
 from enum import unique
 from functools import reduce
 from math import atan, cos, cosh, fabs, pi, sin, sinh, sqrt, tan, tanh
-from typing import NamedTuple
+from typing import NamedTuple, Union
 
 import numpy as np
 from attr import attrs
@@ -13,23 +13,26 @@ from numpy import random
 from scipy.optimize import brentq
 
 from my_research_libs import ideal, qmc_base
-from my_research_libs.qmc_base.utils import min_distance
-from my_research_libs.utils import get_random_rng_seed
+from my_research_libs.qmc_base.utils import min_distance, recast_to_supercell
 
 __all__ = [
     'ArrayGUFunc',
     'ArrayGUPureFunc',
     'CoreFuncs',
     'EnergyGUFunc',
-    'Spec',
     'ModelParams',
     'ModelVarParams',
-    'Sampling',
-    'UniformSampling',
+    'TPFSpecNT',
+    'SamplingFuncs',
     'ScalarGUFunc',
     'ScalarGUPureFunc',
+    'Spec',
+    'UniformSamplingFuncs',
+    'UTPFSpecNT',
     'WFGUFunc',
-    'core_funcs'
+    'core_funcs',
+    'sampling_funcs',
+    'uniform_sampling_funcs'
 ]
 
 # Some alias..
@@ -575,35 +578,110 @@ class CoreFuncs(qmc_base.jastrow.CoreFuncs):
 core_funcs = CoreFuncs()
 
 
-class Sampling(qmc_base.jastrow.vmc.PBCSampling):
-    """Sampling of the probability density of the Bloch-Phonon model with
-    a multi-rods external potential.
+class TPFSpecNT(qmc_base.jastrow.vmc.TPFSpecNT, NamedTuple):
+    """Parameters of the transition probability function.
+
+    The parameters correspond to a sampling done with random numbers
+    generated from a normal (gaussian) distribution function.
+    """
+    boson_number: int
+    time_step: float
+    lower_bound: float
+    upper_bound: float
+
+
+class UTPFSpecNT(qmc_base.jastrow.vmc.UTPFSpecNT, NamedTuple):
+    """Parameters of the transition probability function.
+
+    The parameters correspond to a sampling done with random numbers
+    generated from a uniform distribution function.
+    """
+    boson_number: int
+    move_spread: float
+    lower_bound: float
+    upper_bound: float
+
+
+class SamplingFuncs(qmc_base.jastrow.vmc.SamplingFuncs):
+    """Sampling of the probability density of the Bloch-Phonon model.
+
+    The sampling is subject to periodic boundary conditions due to the
+    multi-rods external potential. The random numbers used in the calculation
+    are generated from a normal (gaussian) distribution function.
     """
 
-    @property
-    def gen_args(self):
-        """The arguments of the sampling generator function.
+    @cached_property
+    def recast(self):
+        """Apply the periodic boundary conditions on a configuration."""
+
+        @jit(nopython=True)
+        def _recast(z: float, tpf_spec: Union[TPFSpecNT, UTPFSpecNT]):
+            """Apply the periodic boundary conditions on a configuration.
+
+            :param z:
+            :param tpf_spec:
+            :return:
+            """
+            z_min = tpf_spec.lower_bound
+            z_max = tpf_spec.upper_bound
+            return recast_to_supercell(z, z_min, z_max)
+
+        return _recast
+
+    @cached_property
+    def ith_sys_conf_tpf(self):
+        """
 
         :return:
         """
-        names = self.params_cls.names
-        rng_seed = self.params[names.RNG_SEED]
-        if rng_seed is None:
-            rng_seed = get_random_rng_seed()
+        pos_slot = int(self.sys_conf_slots.pos)
+        rand_displace = self.rand_displace
+        recast = self.recast  # TODO: Use a better name, maybe?
 
-        return (
-            self.model_spec.cfc_spec_nt,
-            self.ppf_args,
-            self.params[names.INI_SYS_CONF],
-            self.params[names.CHAIN_SAMPLES],
-            self.params[names.BURN_IN_SAMPLES],
-            rng_seed
-        )
+        @jit(nopython=True, cache=True)
+        def _ith_sys_conf_ppf(i_: int,
+                              ini_sys_conf: np.ndarray,
+                              prop_sys_conf: np.ndarray,
+                              tpf_spec: Union[TPFSpecNT, UTPFSpecNT]):
+            """Move the i-nth particle of the current configuration of the
+            system under PBC.
+
+            :param i_:
+            :param ini_sys_conf: The current (initial) configuration.
+            :param prop_sys_conf: The proposed configuration.
+            :param tpf_spec:.
+            :return:
+            """
+            # Unpack data
+            z_i = ini_sys_conf[pos_slot, i_]
+            rnd_spread = rand_displace(tpf_spec)
+            z_i_upd = recast(z_i + rnd_spread, tpf_spec)
+            prop_sys_conf[pos_slot, i_] = z_i_upd
+
+        return _ith_sys_conf_ppf
+
+    @property
+    def wf_abs_log(self):
+        """"""
+        return core_funcs.wf_abs_log
 
 
-class UniformSampling(Sampling, qmc_base.jastrow.vmc.PBCUniformSampling):
-    """"""
+#
+sampling_funcs = SamplingFuncs()
+
+
+class UniformSamplingFuncs(SamplingFuncs, qmc_base.vmc.UniformSamplingFuncs):
+    """Sampling of the probability density of the Bloch-Phonon model.
+
+    The sampling is subject to periodic boundary conditions due to the
+    multi-rods external potential. The random numbers used in the calculation
+    are generated from a uniform distribution function.
+    """
     pass
+
+
+#
+uniform_sampling_funcs = UniformSamplingFuncs()
 
 
 @jit(nopython=True, cache=True)
--- tests/multirods_qmc/test_sampling.py
+++ tests/multirods_qmc/test_sampling.py
@@ -52,22 +52,26 @@ def test_base_sampling():
 
     ncs, nbs = 1000, 0
     ini_sys_conf = model_spec.init_get_sys_conf()
-    sampling_params = dict(move_spread=0.05,
-                           ini_sys_conf=ini_sys_conf,
+    move_spread = 0.05
+    sampling_params = dict(ini_sys_conf=ini_sys_conf,
                            chain_samples=ncs,
                            burn_in_samples=nbs,
                            rng_seed=1)
-    sampling = bloch_phonon.UniformSampling(model_spec, sampling_params)
+    wf_spec = model_spec.cfc_spec_nt
+    ppf_spec = bloch_phonon.UTPFSpecNT(model_spec.boson_number,
+                                       move_spread,
+                                       *model_spec.boundaries)
+    sampling = bloch_phonon.uniform_sampling_funcs
     ar = 0
-    for data in sampling:
+    for data in sampling.generator(wf_spec, ppf_spec, **sampling_params):
         sys_conf, wfv, stat = data
         ar += stat
     ar /= ncs
 
-    chain_data = sampling.as_chain()
+    chain_data = sampling.as_chain(wf_spec, ppf_spec, **sampling_params)
     sys_conf_chain, wf_abs_log_chain, ar_ = chain_data
 
-    assert sys_conf_chain.shape == (ncs, model_spec.num_sys_conf_slots, nop)
+    assert sys_conf_chain.shape == (ncs, len(model_spec.sys_conf_slots), nop)
     assert ar == ar_
 
     wf_abs_log = model_spec.core_funcs.wf_abs_log
