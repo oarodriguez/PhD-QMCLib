13a5e55d934b26e7ae6b949a9164dd0a753841e6
Omar Abel Rodríguez-López <oarodriguez.mx@gmail.com>
2018-12-26 18:11:45 -0600

    ✅  Testing the stats.reblock module

---
+++ tests/stats/test_reblock.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+from my_research_libs.stats.reblock import (
+    Reblocking, StratifiedReblocking, stratified_reblocking
+)
+
+
+def test_stats():
+    """"""
+    size_max_order = 16
+    data_size = 2 ** size_max_order
+    data_sample = np.random.random_sample(data_size)
+
+    block_analysis = Reblocking(data_sample, min_num_blocks=32)
+
+    print(block_analysis.block_sizes)
+    print(block_analysis.num_blocks)
+    print(block_analysis.int_corr_times)
+    print(block_analysis.corr_time_fit.params)
+    # print(block_analysis.corr_time_fit.errors)
+
+    fly_reblocking = stratified_reblocking(data_sample, max_order=3)
+    print(fly_reblocking)
+
+
+def test_stratified_reblocking():
+    """"""
+    size_max_order = 22
+    data_size = 2 ** size_max_order
+    data_sample = np.random.random_sample(data_size)
+
+    raw_strat_reblocking = stratified_reblocking(data_sample)
+    strat_reblocking = StratifiedReblocking(raw_strat_reblocking)
+    strat_reblocking_vars = strat_reblocking.vars
+    print(strat_reblocking_vars)
+
+    reblocking = Reblocking(data_sample, min_num_blocks=2)
+    reblocking_vars = reblocking.vars
+    print(reblocking_vars)
+
+    assert np.allclose(strat_reblocking_vars, reblocking_vars)
