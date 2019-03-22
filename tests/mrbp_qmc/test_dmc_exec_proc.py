906b3e4f9b2633d4d17ea020bb81e9b16dabd00f
Omar Abel Rodríguez-López <oarodriguez.mx@gmail.com>
2019-03-22 16:33:07 -0600

    ✔  Updating tests

--- tests/mrbp_qmc/test_dmc_exec_proc.py
+++ tests/mrbp_qmc/test_dmc_exec_proc.py
@@ -1,41 +1,102 @@
+import pathlib
+
 from matplotlib import pyplot
 
-from my_research_libs.mrbp_qmc import Spec, dmc_exec, vmc_exec
+from my_research_libs.constants import ER
+from my_research_libs.mrbp_qmc import Spec, dmc_exec
+
+lattice_depth = 5 * ER
+lattice_ratio = 1
+interaction_strength = 2
+boson_number = 8
+supercell_size = 8
+tbf_contact_cutoff = 0.25 * supercell_size
+
+# TODO: Improve this test.
+model_spec = Spec(lattice_depth=lattice_depth,
+                  lattice_ratio=lattice_ratio,
+                  interaction_strength=interaction_strength,
+                  boson_number=boson_number,
+                  supercell_size=supercell_size,
+                  tbf_contact_cutoff=tbf_contact_cutoff)
 
 
 def test_proc():
     """Testing the main task to realize a DMC calculation."""
-
-    lattice_depth = 0
-    lattice_ratio = 1
-    interaction_strength = 4
-    boson_number = 8
-    supercell_size = 8
-    tbf_contact_cutoff = 0.25 * supercell_size
-
-    # TODO: Improve this test.
-    model_spec = Spec(lattice_depth=lattice_depth,
-                      lattice_ratio=lattice_ratio,
-                      interaction_strength=interaction_strength,
-                      boson_number=boson_number,
-                      supercell_size=supercell_size,
-                      tbf_contact_cutoff=tbf_contact_cutoff)
-    move_spread = 0.25 * model_spec.well_width
-    ini_sys_conf = model_spec.init_get_sys_conf()
-    rng_seed = None
-    num_batches = 8
-    num_steps_batch = 4096
-    # num_steps = num_batches * num_steps_batch
-    vmc_proc = vmc_exec.Proc(model_spec,
-                             move_spread,
-                             rng_seed=rng_seed,
-                             num_batches=num_batches,
-                             num_steps_batch=num_steps_batch)
-
-    time_step = 1e-3
+    time_step = 6.25e-4
     num_batches = 4
+    num_time_steps_batch = 4096
+    target_num_walkers = 480
+    max_num_walkers = 512
+    # ini_ref_energy = None
+    rng_seed = None
+
+    dmc_proc = \
+        dmc_exec.Proc(model_spec,
+                      time_step,
+                      max_num_walkers,
+                      target_num_walkers,
+                      rng_seed=rng_seed,
+                      num_batches=num_batches,
+                      num_time_steps_batch=num_time_steps_batch)
+
+    sys_conf_spec = dmc_exec.ModelSysConfSpec(dist_type='RANDOM')
+    dmc_proc_input = \
+        dmc_exec.ProcInput.from_model_sys_conf_spec(sys_conf_spec, dmc_proc)
+    dmc_result = dmc_proc.exec(dmc_proc_input)
+
+    energy_mean = dmc_result.data.blocks.energy.mean
+    print(energy_mean)
+
+
+def test_density_proc():
+    """Testing the calculation of the density."""
+    time_step = 6.25e-4
+    num_batches = 2
     num_time_steps_batch = 512
-    # ini_sys_conf_set = None
+    target_num_walkers = 480
+    max_num_walkers = 512
+    # ini_ref_energy = None
+    rng_seed = None
+
+    num_bins = supercell_size * 32
+    density_spec = dmc_exec.DensityEstSpec(num_bins=num_bins)
+    dmc_proc = \
+        dmc_exec.Proc(model_spec,
+                      time_step,
+                      max_num_walkers,
+                      target_num_walkers,
+                      rng_seed=rng_seed,
+                      num_batches=num_batches,
+                      num_time_steps_batch=num_time_steps_batch,
+                      density_spec=density_spec)
+
+    sys_conf_spec = dmc_exec.ModelSysConfSpec(dist_type='RANDOM')
+    dmc_proc_input = \
+        dmc_exec.ProcInput.from_model_sys_conf_spec(sys_conf_spec, dmc_proc)
+    result = dmc_proc.exec(dmc_proc_input)
+
+    bin_size = supercell_size / num_bins
+    bins_edges = dmc_proc.sampling.density_bins_edges[:-1]
+    density_mean = result.data.blocks.density.mean / bin_size
+
+    pyplot.plot(bins_edges, density_mean)
+    pyplot.xlabel(r'$z / (a + b)$')
+    pyplot.ylabel(r'$n(z)$')
+    pyplot.show()
+
+    h5f_path = pathlib.Path('./test-density-results.h5')
+    if h5f_path.exists():
+        h5f_path.unlink()
+    handler = dmc_exec.HDF5FileHandler(h5f_path, 'density-data-group')
+    handler.dump(result)
+
+
+def test_ssf_proc():
+    """Testing the calculation of the static structure factor."""
+    time_step = 6.25e-4
+    num_batches = 2
+    num_time_steps_batch = 512
     target_num_walkers = 480
     max_num_walkers = 512
     # ini_ref_energy = None
@@ -56,12 +117,21 @@ def test_proc():
     sys_conf_spec = dmc_exec.ModelSysConfSpec(dist_type='RANDOM')
     dmc_proc_input = \
         dmc_exec.ProcInput.from_model_sys_conf_spec(sys_conf_spec, dmc_proc)
-    dmc_result = dmc_proc.exec(dmc_proc_input)
+    result = dmc_proc.exec(dmc_proc_input)
 
-    ssf_momenta = dmc_result.proc.sampling.ssf_momenta
-    ss_factor_mean = dmc_result.data.blocks.ss_factor.mean
-    pyplot.plot(ssf_momenta, ss_factor_mean)
+    ssf_momenta = dmc_proc.sampling.ssf_momenta
+    ssf_mean = result.data.blocks.ss_factor.mean
+
+    pyplot.plot(ssf_momenta, ssf_mean)
+    pyplot.xlabel(r'$k / n$')
+    pyplot.ylabel(r'$S(k)$')
     pyplot.show()
+
+    h5f_path = pathlib.Path('./test-ssf-results.h5')
+    if h5f_path.exists():
+        h5f_path.unlink()
+    handler = dmc_exec.HDF5FileHandler(h5f_path, 'ssf-data-group')
+    handler.dump(result)
 
 
 if __name__ == '__main__':
